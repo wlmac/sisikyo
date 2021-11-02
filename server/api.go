7576699a0519734c0aed8d61d65b8e19edf5edcf
Ken Shibata <kenxshibata@gmail.com>
2021-11-02 09:09:22 -0400

    server: fix ignored error

--- server/api.go
+++ server/api.go
@@ -5,30 +5,35 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"net/url"
+	"time"
 
 	"gitlab.com/mirukakoro/sisikyo/events/api"
 )
 
 var apiURL string
+var apiTimeout time.Duration
 
 func init() {
-	flag.StringVar(&apiURL, "api-url", "", "URL of API to use")
+	flag.StringVar(&apiURL, "api-url", "", "api: base URL")
+	flag.DurationVar(&apiTimeout, "api-timeout", 1*time.Second, "api: response timeout")
 }
 
-func setupAPI() error {
+func setupAPI() (*api.Client, error) {
 	if apiURL == "" {
-		return errors.New("api-url: cannot be blank")
+		return nil, errors.New("api-url: cannot be blank")
 	}
-	var err error
-	api.DefaultBaseURL, err = url.Parse(apiURL)
+	baseURL, err := url.Parse(apiURL)
 	if err != nil {
-		return fmt.Errorf("api-url: %w", err)
+		return nil, fmt.Errorf("api-url: %w", err)
 	}
-
-	cl := api.DefaultClient()
+	cl := api.NewClient(&http.Client{Timeout: apiTimeout}, baseURL)
 	resp := api.VersionResp{}
-	cl.Do(api.VersionReq{}, &resp)
+	err = cl.Do(api.VersionReq{}, &resp)
+	if err != nil {
+		return nil, fmt.Errorf("api version: %w", err)
+	}
 	log.Printf("api: conn'd (version: %s)", resp.Version)
-	return nil
+	return cl, nil
 }
