81f3d686e71c978e8413e3c8e7e61f39e7a9cb0f
Ken Shibata <kenxshibata@gmail.com>
2021-11-04 15:24:00 -0400

    format license text

--- server/static.go
+++ server/static.go
@@ -26,7 +26,8 @@ Copyright 2021 Ken Shibata
 
 This program comes with ABSOLUTELY NO WARRANTY and this program is free software, and you are welcome to redistribute it under certain conditions; for details view the [/license](/license) page or view the ` + "`license.md`" + ` file.
 This program uses open source software; for details view the [/license](/license) page or view the ` + "`license.md`" + ` file.
-The source code is available from the [/src](/src) page.`
+The source code is available from the [/src](/src) page.
+`
 
 //go:embed license.md
 var licenseFull string
