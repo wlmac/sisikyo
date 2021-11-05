2fdc1583bef582bf9b55df12600bd39e1de76ee0
Ken Shibata <kenxshibata@gmail.com>
2021-11-04 23:57:05 -0400

    prevent multiple redirects using the same state

--- server/handlers.go
+++ server/handlers.go
@@ -108,6 +108,7 @@ func (e *engineContext) OauthRedirect(c *gin.Context) {
 		c.String(http.StatusBadRequest, "state get: failed")
 		return
 	}
+	oauthClearState(c)
 	var params oauth.RedirectParams
 	if err := c.Bind(&params); err != nil {
 		c.String(http.StatusBadRequest, "params get: failed")
@@ -152,7 +153,7 @@ func (e *engineContext) OauthRedirect(c *gin.Context) {
 		c.String(http.StatusInternalServerError, fmt.Sprint(err))
 		return
 	}
-	c.String(http.StatusOK, user.Control)
+	c.HTML(http.StatusOK, "registered.html", gin.H{"control": user.Control})
 }
 
 // OauthAuthorize redirects the user to the OAuth server to authorize this OAuth client.
@@ -169,12 +170,37 @@ func (e *engineContext) OauthAuthorize(c *gin.Context) {
 // oauthStateName is the name of the cookie to store the OAuth state.
 const oauthStateName = "oauth-state" // set __Host- prefix only if secure
 
+// oauthClearState sets a Set-Cookie header to set a state cookie.
+func oauthClearState(c *gin.Context) {
+	// why not use `Max-Age: 0`?
+	// 	see <https://www.rfc-editor.org/errata/eid3430> which is part of <https://stackoverflow.com/a/20320610>
+	if gin.Mode() == "debug" {
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:     oauthStateName,
+			Value:    "",
+			Path:     "/o",
+			MaxAge:   1, // 1 second
+			SameSite: http.SameSiteLaxMode,
+		})
+	} else {
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:     oauthStateName,
+			Value:    "",
+			Path:     "/o",
+			MaxAge:   1, // 1 second
+			Secure:   true,
+			SameSite: http.SameSiteStrictMode,
+		})
+	}
+}
+
 // oauthSetState sets a Set-Cookie header to set a state cookie.
 func oauthSetState(c *gin.Context, state string) {
 	if gin.Mode() == "debug" {
 		http.SetCookie(c.Writer, &http.Cookie{
 			Name:     oauthStateName,
 			Value:    state,
+			Path:     "/o",
 			SameSite: http.SameSiteLaxMode,
 		})
 	} else {
